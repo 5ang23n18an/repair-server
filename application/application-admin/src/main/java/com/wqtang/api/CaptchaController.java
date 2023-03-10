104790fab92abc616f327797b1a61f81b0ab0e58
wqtang <twq_wppp@163.com>
2023-03-10 11:26:00 +0800

    feat(captcha): add Logger in CaptchaController

--- application/application-admin/src/main/java/com/wqtang/api/CaptchaController.java
+++ application/application-admin/src/main/java/com/wqtang/api/CaptchaController.java
@@ -6,6 +6,8 @@ import com.wqtang.exception.BusinessException;
 import com.wqtang.object.enumerate.ErrorEnum;
 import com.wqtang.object.enumerate.SystemConfigKeyEnum;
 import com.wqtang.object.vo.response.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +19,8 @@ import javax.annotation.Resource;
  */
 @RestController
 public class CaptchaController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CaptchaController.class);
 
     @Resource(name = "captchaService")
     private CaptchaService captchaService;
@@ -31,7 +35,8 @@ public class CaptchaController {
         try {
             return captchaService.getCaptchaImage();
         } catch (Exception e) {
-            throw new BusinessException(ErrorEnum.ERROR, e.getMessage());
+            LOGGER.error("Exception occurs in `CaptchaController.getCaptchaImage`, error message is {}", e.getMessage(), e);
+            throw new BusinessException(ErrorEnum.ERROR, "Failed to get captcha image", "获取验证码图片失败");
         }
     }
 
