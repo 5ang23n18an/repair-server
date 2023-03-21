1852fce5c3835724e1c9d467faa460ad39cf7c83
wqtang <twq_wppp@163.com>
2023-03-21 22:41:49 +0800

    comment(pager): add comments on AbstractPager

--- commons/src/main/java/com/wqtang/controller/AbstractPager.java
+++ commons/src/main/java/com/wqtang/controller/AbstractPager.java
@@ -10,15 +10,38 @@ import java.util.List;
  */
 public abstract class AbstractPager<T> {
 
+    /**
+     * 分页查询
+     *
+     * @param request
+     * @param pageNumber
+     * @param pageSize
+     * @return
+     */
     public PageInfo getPage(T request, int pageNumber, int pageSize) {
         int total = getPageTotal(request);
         int offset = (pageNumber - 1) * pageSize;
+        // limit {offset}, {pageSize}
         List<?> records = getPageRecords(request, offset, pageSize);
         return new PageInfo(pageNumber, pageSize, total, records);
     }
 
+    /**
+     * 查询当前条件下, 共有多少条数据
+     *
+     * @param request
+     * @return
+     */
     public abstract int getPageTotal(T request);
 
+    /**
+     * 获取当前分页的数据集合
+     *
+     * @param request
+     * @param offset
+     * @param limit
+     * @return
+     */
     public abstract List<?> getPageRecords(T request, int offset, int limit);
 
 }
