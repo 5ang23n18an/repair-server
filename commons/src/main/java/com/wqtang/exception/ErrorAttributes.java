d44477f768c3710eb8e2b4dc35d95e179805919b
wqtang <twq_wppp@163.com>
2023-04-21 22:19:16 +0800

    修改(controller): errorAttributes的path属性值

--- commons/src/main/java/com/wqtang/exception/ErrorAttributes.java
+++ commons/src/main/java/com/wqtang/exception/ErrorAttributes.java
@@ -1,10 +1,10 @@
 package com.wqtang.exception;
 
-import org.apache.commons.lang3.StringUtils;
+import com.google.common.collect.Maps;
+import com.wqtang.util.ServletUtils;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.Map;
@@ -17,7 +17,7 @@ import java.util.Map;
 public class ErrorAttributes extends DefaultErrorAttributes {
 
     /**
-     * errorAttributes: timestamp, path, error, errorCode, message
+     * 包含6类信息: timestamp, path, error, errorCode, message
      *
      * @param webRequest
      * @param options
@@ -25,12 +25,9 @@ public class ErrorAttributes extends DefaultErrorAttributes {
      */
     @Override
     public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
-        Map<String, Object> errorAttributes = CollectionUtils.newHashMap(6);
+        Map<String, Object> errorAttributes = Maps.newLinkedHashMapWithExpectedSize(6);
         long timestamp = System.currentTimeMillis();
-        String path = (String) webRequest.getAttribute("javax.servlet.error.request_uri", 0);
-        if (StringUtils.isBlank(path)) {
-            path = webRequest.getContextPath();
-        }
+        String path = ServletUtils.getHttpServletRequest().getRequestURI();
         Throwable exception = getError(webRequest);
         BusinessException businessException = exception instanceof BusinessException ? ((BusinessException) exception) : ((BusinessException) exception.getCause());
         String errorStr = businessException.getErrorEnum().name();
