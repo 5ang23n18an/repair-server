20a8ac59456af98628afddbcfce7e7b14dac7353
wqtang <twq_wppp@163.com>
2023-05-02 18:40:29 +0800

    开发(monitor): 新增MonitorOnlineUserController

---
+++ application/application-repair/src/main/java/com/wqtang/api/monitor/MonitorOnlineUserController.java
@@ -0,0 +1,108 @@
+package com.wqtang.api.monitor;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.google.common.collect.Lists;
+import com.wqtang.object.annotation.OperationLog;
+import com.wqtang.object.domain.user.OnlineUser;
+import com.wqtang.object.enumerate.BusinessType;
+import com.wqtang.object.enumerate.OperatorType;
+import com.wqtang.object.enumerate.RedisKeyEnum;
+import com.wqtang.object.po.user.LoginUser;
+import com.wqtang.util.RedisUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Wenqian Tang
+ * @date 2023/5/2
+ */
+@RestController
+@RequestMapping("/monitor/online")
+public class MonitorOnlineUserController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MonitorOnlineUserController.class);
+
+    @Resource(name = "redisUtils")
+    private RedisUtils redisUtils;
+
+    /**
+     * 获取当前在线用户信息列表
+     *
+     * @param ipaddr
+     * @param username
+     * @param pageNumber
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("/page")
+    public PageInfo<OnlineUser> getPage(String ipaddr, String username,
+                                        @RequestParam(required = false, defaultValue = "1", value = "pageNumber") int pageNumber,
+                                        @RequestParam(required = false, defaultValue = "20", value = "pageSize") int pageSize) {
+        LOGGER.info("ipaddr = {}, username = {},\r\npageNumber = {}, pageSize = {}", ipaddr, username, pageNumber, pageSize);
+        PageHelper.startPage(pageNumber, pageSize);
+        List<OnlineUser> list = Lists.newArrayList();
+        for (String key : redisUtils.keysMatchPrefix(RedisKeyEnum.LOGIN_TOKEN.getPrefix())) {
+            LoginUser loginUser = redisUtils.getAndCast(key, LoginUser.class);
+            if (loginUser == null) {
+                continue;
+            }
+            if (StringUtils.isAllBlank(ipaddr, username)) {
+                list.add(convertLoginUserToOnlineUser(loginUser));
+                continue;
+            }
+            if (StringUtils.isNoneBlank(ipaddr, username)) {
+                if (ipaddr.equals(loginUser.getIpAddr()) && username.equals(loginUser.getUsername())) {
+                    list.add(convertLoginUserToOnlineUser(loginUser));
+                }
+                continue;
+            }
+            if (StringUtils.isNotBlank(ipaddr)) {
+                if (ipaddr.equals(loginUser.getIpAddr())) {
+                    list.add(convertLoginUserToOnlineUser(loginUser));
+                }
+                continue;
+            }
+            if (StringUtils.isNotBlank(username)) {
+                if (username.equals(loginUser.getUsername())) {
+                    list.add(convertLoginUserToOnlineUser(loginUser));
+                }
+            }
+        }
+        Collections.reverse(list);
+        return new PageInfo<>(list);
+    }
+
+    private OnlineUser convertLoginUserToOnlineUser(LoginUser loginUser) {
+        OnlineUser onlineUser = new OnlineUser();
+        onlineUser.setToken(loginUser.getToken());
+        onlineUser.setUsername(loginUser.getUsername());
+        if (loginUser.getUser() != null && loginUser.getUser().getDept() != null) {
+            onlineUser.setDeptName(loginUser.getUser().getDept().getDeptName());
+        }
+        onlineUser.setIpaddr(loginUser.getIpAddr());
+        onlineUser.setLoginLocation(loginUser.getLoginLocation());
+        onlineUser.setBrowser(loginUser.getBrowser());
+        onlineUser.setOs(loginUser.getOs());
+        onlineUser.setLoginTime(loginUser.getLoginTime());
+        return onlineUser;
+    }
+
+    /**
+     * 强退用户
+     *
+     * @param token
+     */
+    @DeleteMapping("/forceLogout")
+    @OperationLog(title = "在线用户", businessType = BusinessType.FORCE, operatorType = OperatorType.ADMIN)
+    public void forceLogout(String token) {
+        redisUtils.delete(RedisUtils.getRedisKey(RedisKeyEnum.LOGIN_TOKEN, token));
+    }
+
+}
---
+++ commons/src/main/java/com/wqtang/object/domain/user/OnlineUser.java
@@ -0,0 +1,115 @@
+package com.wqtang.object.domain.user;
+
+import java.util.Date;
+
+/**
+ * @author Wenqian Tang
+ * @date 2023/5/2
+ */
+public class OnlineUser {
+
+    /**
+     * 会话编号
+     */
+    private String token;
+
+    /**
+     * 用户名称
+     */
+    private String username;
+
+    /**
+     * 部门名称
+     */
+    private String deptName;
+
+    /**
+     * 登录IP地址
+     */
+    private String ipaddr;
+
+    /**
+     * 登录地址
+     */
+    private String loginLocation;
+
+    /**
+     * 浏览器类型
+     */
+    private String browser;
+
+    /**
+     * 操作系统
+     */
+    private String os;
+
+    /**
+     * 登录时间
+     */
+    private Date loginTime;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+    public String getIpaddr() {
+        return ipaddr;
+    }
+
+    public void setIpaddr(String ipaddr) {
+        this.ipaddr = ipaddr;
+    }
+
+    public String getLoginLocation() {
+        return loginLocation;
+    }
+
+    public void setLoginLocation(String loginLocation) {
+        this.loginLocation = loginLocation;
+    }
+
+    public String getBrowser() {
+        return browser;
+    }
+
+    public void setBrowser(String browser) {
+        this.browser = browser;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    public Date getLoginTime() {
+        return loginTime;
+    }
+
+    public void setLoginTime(Date loginTime) {
+        this.loginTime = loginTime;
+    }
+
+}
